c8ca05363c814ed6d6b01dcdb26ab9848ad2d075
Hugo <hpeters1024@gmail.com>
2022-04-14 14:59:33 +0200

    delete back to skeleton in prep of newer version

--- hs-comprehension/scripts/diff.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-git diff --no-index --color --ignore-all-space --no-color-moved $1 $2 | ansi2html --partial
--- hs-comprehension/src/CoreLang/Utils.hs
+++
@@ -1,6 +0,0 @@
-module CoreLang.Utils where
-
-import CoreLang.Types
-
-coreLangBindBndr :: CoreBind -> CoreId
-coreLangBindBndr (NonRec b _) = b
